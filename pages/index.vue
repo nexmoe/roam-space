4f7769dcad36b063373d6639348859ab622d16b5
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-04-24 11:10:46 +0800

    feat: catalog

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@ const { $client } = useNuxtApp()
 const flows = await $client.flow.list.query()
 
 const config = await useGetConfig()
+const globalStore = useGlobalStore()
+
+globalStore.setCatalog(flows.map(x => ({ title: x.title, anchor: x.title, active: false })))
 
 provide('flows', flows)
 
@@ -25,10 +28,8 @@ defineOgImageComponent('NuxtSeo', {
 			<PublicProse :title="`Hi, I'm ${config.siteName}`">
 				<div class="whitespace-pre-wrap" v-html="config.description" />
 				<div class="tags flex flex-row flex-wrap gap-2">
-					<div
-						v-for="item in config.tags" :key="item"
-						class="text-black inline-block shu-card border-none text-sm px-3 py-0.5"
-					>
+					<div v-for="item in config.tags" :key="item"
+						class="text-black inline-block shu-card border-none text-sm px-3 py-0.5">
 						{{ item }}
 					</div>
 				</div>
---
+++ stores/useGlobalStore.ts
@@ -0,0 +1,20 @@
+interface Catalog {
+    title: string;
+    anchor: string;
+    active: boolean;
+}
+
+interface State {
+    catalog: Catalog[]
+}
+
+export default defineStore("global", {
+    state: () => <State>({
+        catalog: [],
+    }),
+    actions: {
+        setCatalog(catalog: Catalog[]) {
+            this.catalog = catalog
+        }
+    },
+})
