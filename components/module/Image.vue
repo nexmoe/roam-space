d3df9677f27f6565557b906a1bde9ed2cc55ea7f
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-05-02 13:22:17 +0800

    refactor: Update image sizes and link icon source
    - Decrease image width to 500px in Gallery.vue
    - Update LinkIcon source to unavatar.io in LinkIcon.vue
    - Remove rounded styling from link icon in Image.vue

--- components/module/Image.vue
+++ components/module/Image.vue
@@ -20,7 +20,7 @@ const text = computed(() => extractTextFromHTML(props.module.content))
 	<div class="module shu-card p-3 gap-1 flex flex-col justify-between">
 		<div class="space-y-4">
 			<div class="flex flex-row items-center pt-1 gap-2 px-2.5">
-				<div class="w-5 h-5 block rounded-full overflow-hidden">
+				<div class="w-5 h-5 block overflow-hidden">
 					<LinkIcon :url="props.module.url" />
 				</div>
 				<div>
